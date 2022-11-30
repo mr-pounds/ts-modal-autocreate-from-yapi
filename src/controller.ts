a3c2c69cb03bddbc7894719b29d4bd4d435c4a37
zzz <740870608@qq.com>
2022-11-30 17:33:01 +0800

    mod 增加进度条

--- src/controller.ts
+++ src/controller.ts
@@ -4,7 +4,7 @@
  * @Author       : zzz
  * @Date         : 2022-11-29 15:13:29
  * @LastEditors  : zzz
- * @LastEditTime : 2022-11-30 16:34:46
+ * @LastEditTime : 2022-11-30 17:13:58
  */
 
 import * as vscode from "vscode";
@@ -30,5 +30,33 @@ export async function fetcher(args: any) {
     return;
   }
 
-  // 获取需要生成模型的接口数据，按接口顺序执行，获取一个生成一个。
+  // 开始在后端执行
+  await vscode.window.withProgress(
+    {
+      cancellable: false,
+      location: vscode.ProgressLocation.Notification,
+      title: "create YApi modal file",
+    },
+    (progress) => {
+      // 先校验是否存在对应的文件，不存在便创建；
+      progress.report({
+        increment: 1,
+        message: "正在创建ts文件",
+      });
+
+      choosedAPiList?.forEach(async (api, index) => {
+        // 获取接口数据
+        // 生成接口请求参数的模型并返回
+        // 生成公共模型并返回
+        // 生成响应参数的模型并返回
+        // 生成接口
+        progress.report({
+          increment: (index / choosedAPiList.length) * 100,
+          message: `正在创建${api.title}相关模型及接口`,
+        });
+      });
+      // 获取需要生成模型的接口数据，按接口顺序执行，获取一个生成一个。
+      return new Promise(async (resolve) => resolve(null));
+    }
+  );
 }
